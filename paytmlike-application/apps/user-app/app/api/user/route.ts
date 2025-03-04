8a635ef3caecad216b8fc9a5efe67a3a4aafc6e3
SHIVAMTANK <shivamtank8059@gmail.com>
2025-03-04 09:10:52 +0530

    adding recoil and prisma

--- paytmlike-application/apps/user-app/app/api/user/route.ts
+++ paytmlike-application/apps/user-app/app/api/user/route.ts
@@ -1,4 +1,6 @@
+"use client"
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@repo/db/client";
+// import {useBalance} from "@repo/store/balance"
 
 const client = new PrismaClient();
